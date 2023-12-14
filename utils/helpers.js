f0098a7fd532b3d262a7ed89c066f58e9df3f566
AgrastP <agrastp@yahoo.com>
2023-12-14 14:19:34 -0500

    login handlebars file

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,32 @@
+const {v4: uuidv4} = require('uuid');
+
 module.exports = {
-    format_time: (date) => {
-      return date.toLocaleTimeString();
+
+    count: (index) => {
+
+        return index + 1;
     },
+
+    equals: (argumentOne, argumentTwo) => {
+
+        if(argumentOne === argumentTwo){
+
+            return true;
+        
+        } else {
+
+            return false;
+        }
+    },
+
+    generateUuid:() => {
+
+        let uuid = uuidv4();
+        return uuid;
+    },
+
+    // to change the date format
     format_date: (date) => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-        new Date(date).getFullYear() + 5
-      }`;
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
     },
-  };
-  
+}
